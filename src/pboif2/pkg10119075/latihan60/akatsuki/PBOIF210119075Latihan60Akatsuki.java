99f9fcf1a814d4c79472e3adde034286d71f9acd
aditmk1 <aditiyamk1010@gmail.com>
2020-11-22 12:10:11 +0700

    STIL running

---
+++ src/pboif2/pkg10119075/latihan60/akatsuki/PBOIF210119075Latihan60Akatsuki.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119075.latihan60.akatsuki;
+
+/**
+Nama : Aditiya musthafa kamil
+Kelas : PBOIF2
+NIM : 10119075
+Deskripsi Program : Program ini berisi program untuk menampilkan akatsuki utama
+ */
+public class PBOIF210119075Latihan60Akatsuki {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Member akatsuki = new Member();
+        akatsuki.tampilProfil();
+    }
+}
